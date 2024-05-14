1956691d291daedec8f7f7c0bff732489425c1d4
VishalMSahani <vishalsahani0808@gmail.com>
2024-05-14 17:17:22 +0530

    add authJS

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,82 @@
+    import { NextAuthOptions } from "next-auth";
+    import  CredentialsProvider from "next-auth/providers/credentials";
+    import bcrypt from "bcryptjs"
+    import { dbConnect } from "@/lib/dbConnect";
+    import UserModel from "@/models/User";
+
+    export const authOption: NextAuthOptions= {
+        providers:[
+            CredentialsProvider({
+                id: "credentials",
+                name: "Credentials",
+                credentials: {
+                    email: { label: "email", type: "text"},
+                    password: { label: "Password", type: "password" }
+                  },
+                  async authorize(credentials:any ) : Promise<any>{
+                    await dbConnect()
+                    try {
+                        const user = await UserModel.findOne({
+                            $or:[
+                                {email: credentials.identifier},
+                                {username: credentials.identifier}
+                            ]
+                        })
+
+                        if (!user) {
+                            throw new Error("No User found with this email")
+                        }
+                        if (!user.isVerified) {
+                            throw new Error("Please verify your email first to login with this email")
+                        }
+
+                        const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+
+                        if (isPasswordCorrect) {
+                            return user
+                        } else {
+                            throw new Error("Incorrect password, please provide valid password")
+                        }
+                        
+                    } catch (error: any){
+                        throw new Error(error)
+                    }
+                  }
+            })
+        ],
+
+        callbacks:{
+            async jwt({ token, user, }) {
+
+                if (user) {
+                    token._id =user._id?.toString()
+                    token.isVerified = user.isVerified;
+                    token.isAcceptingMessages = user.isAcceptingMessages
+                    token.username =user.username
+                }
+
+              return token
+            },
+            async session({ session, token }) {
+
+                if (token) {
+                    session.user._id =token._id?.toString()
+                    session.user.isVerified = token.isVerified;
+                    session.user.isAcceptingMessages = token.isAcceptingMessages
+                    session.user.username =token.username
+                }
+
+                return session
+              } 
+        },
+
+        pages:{
+            signIn:"/sign-in"
+        },
+
+        session:{
+            strategy: "jwt"
+        },
+
+        secret: process.env.NEXTAUTH_SECRET,
+    }
---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,6 @@
+import NextAuth from "next-auth/next";
+import { authOption } from "./options";
+ 
+const handler = NextAuth(authOption)
+
+export {handler as GET, handler as POST}

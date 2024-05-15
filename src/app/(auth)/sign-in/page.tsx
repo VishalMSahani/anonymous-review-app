5d64c63d881ce3ee6a51cc77ec9007ff73188076
VishalMSahani <vishalsahani0808@gmail.com>
2024-05-15 08:56:43 +0530

    add middleware

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button 
+        onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button className="px-3 py-2 rounded-lg bg-white text-black"
+      onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}

6d637634220030596aa8cbf2eb74203d362d913f
VishalMSahani <vishalsahani0808@gmail.com>
2024-06-01 22:06:16 +0530

    add delete route

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,64 @@
+import UserModel from "@/models/User";
+import { getServerSession } from "next-auth/next";
+import { authOption } from "../../auth/[...nextauth]/options";
+import { dbConnect } from "@/lib/dbConnect";
+import { User } from "next-auth";
+
+export async function DELETE(request:Request, 
+    {params}: {params:{messageid:string}}
+) {
+    const messageId = params.messageid;
+    await dbConnect()
+    const session = await getServerSession(authOption);
+    const _user:User = session?.user as User;
+    if (!session || !_user) {
+        return Response.json(
+            {
+                success:false,
+                message:"User not authenticated"
+            },{
+                status:401
+            }
+        );
+    }
+
+    try {
+
+        const updateResult = await UserModel.updateOne(
+            {_id: _user._id},
+            {$pull:{messages:{_id:messageId}}}
+        )
+
+        if (updateResult.modifiedCount === 0) {
+            return Response.json(
+                {
+                    success:false,
+                    message:"Message not found or already deleted"
+                },{
+                    status:404
+                }
+            );
+        }
+
+        return Response.json(
+            {
+                success:true,
+                message:"Message deleted successfully"
+            },{
+                status:200
+            }
+        );
+        
+    } catch (error) {
+        console.error("Error in deleting message", error)
+        return Response.json(
+            {
+                success:false,
+                message:"Error in while deleting messsage"
+            },{
+                status:501
+            }
+        )
+
+    }
+}
